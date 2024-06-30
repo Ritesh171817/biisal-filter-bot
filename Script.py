921660476a4ce13ae68dbe1d478d16e1b1b7d247
Ritesh171817 <retesh1718@gmail.com>
2024-06-30 19:43:18 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -105,7 +105,7 @@ User - {}"""
 ❗️ ʟᴀɴɢᴜᴀɢᴇ - {languages}</b>
 """
 
-    FILE_CAPTION = """<a href='https://t.me/RS_Movie_1'><b>ɴᴀᴍᴇ : {file_name}</b></a>"""
+    FILE_CAPTION = """<a href='https://t.me/RS_Movie'><b>ɴᴀᴍᴇ : {file_name}</b></a>"""
 
     RESTART_TXT = """<b>
 📅 Dᴀᴛᴇ : <code>{}</code>
