98849c0ade6f9f00e48f0656a2c8bac3349996ae
Ritesh171817 <retesh1718@gmail.com>
2024-06-30 18:32:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ USERNAME = environ.get('USERNAME', "https://telegram.me/Anonymous_7")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002055918522'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/Rasmovierequest')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002238185656').split()]
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://username:password@cluster0.jhvfd.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ryryrymln88808:6CrFpGCUpkRvjBbZ@cluster0.j2xo87f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 DATABASE_NAME = environ.get('DATABASE_NAME', "TELEGRAM_BOT_INFO")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
