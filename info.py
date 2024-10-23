5b955a2c66d8f8fc8a389b7910ed1c0781f23d01
Ritesh171817 <retesh1718@gmail.com>
2024-10-23 17:04:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ API_ID = int(environ.get('API_ID', '25848289'))
 API_HASH = environ.get('API_HASH', '88cf2c17c7a2bd21f4204c89c648dd40')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1557042262 6482751939').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1557042262').split()]
 USERNAME = environ.get('USERNAME', "https://telegram.me/Anonymous_7")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002055918522'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/Rasmovierequest')
